5f0432a4f8a454cf1a86a7f004748922aedee271
Santiago1470 <67245823+Santiago1470@users.noreply.github.com>
2022-10-17 21:08:57 -0500

    Add files via upload

--- js/js-general.js
+++ js/js-general.js
@@ -24,3 +24,25 @@ function cerrarMenu() {
 function animacionDeCerrarMenu() {
     contenedorMenu = document.getElementById("contenedor-menu-desplegable").style.display = "none";
 }
+
+var opcionVerMas = document.getElementById("nav-mas");
+var contador = 0;
+
+opcionVerMas.addEventListener("click", abrirMenuMas);
+
+function abrirMenuMas() {
+    contador = contador + 1;
+
+    if (contador % 2 == 0) {
+        contenedorMenuMas = document.getElementById("menu-mas").style.transform = "translateY(-50px)";
+        contenedorMenuMas = document.getElementById("menu-mas").style.transition = "ease 500ms";    
+
+        flechaArriba = document.getElementById("flecha-arriba-menu-mas").style.display = "none";
+        flechaArriba = document.getElementById("flecha-abajo-menu-mas").style.display = "block";
+    } else {
+        contenedorMenuMas = document.getElementById("menu-mas").style.transform = "translateY(50px)";
+        contenedorMenuMas = document.getElementById("menu-mas").style.transition = "all ease 500ms";
+        flechaAbajo = document.getElementById("flecha-arriba-menu-mas").style.display = "block";
+        flechaAbajo = document.getElementById("flecha-abajo-menu-mas").style.display = "none";
+    }
+}
